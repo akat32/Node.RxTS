1e6e2b3e943115c1137aa441a9af9f70b6884fc3
akat32 <baduk32127@gmail.com>
2019-05-19 03:39:01 +0900

    add cors & body-parser

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,15 @@
 import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import * as cors from 'cors'
+
 import { fromEvent } from 'rxjs'
 
 const app = express()
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '1gb', extended: false }));
+
 
 
 
